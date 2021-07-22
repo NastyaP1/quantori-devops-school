7e099bb57a330925b025d1f87fa08d3577c769f0
NastyP1 <anastasiapereslavtseva@gmail.com>
2021-07-22 17:21:09 +0300

    add task 4 to linux administration

---
+++ Linux_Administration/hw4/Report.md
@@ -0,0 +1,20 @@
+## Описание
+
+В папке /var/ftp/ - есть подпапки пользователей (/var/ftp/ivan, /var/ftp/aleksei, etc.), куда они складывают свои файлы.
+Есть пользователь и группа ftp-admin - они используются администратором ftp сервера, и он может управлять содержимым всех подпапок всех пользователей. При этом обычные пользователи могут управлять только своими файлами.
+
+Написать скрипт для добавления новых пользователей ftp-сервера в систему. Скрипт должен генерировать пароль, добавлять пользователя в систему, настраивать права пользователя, создавать домашний каталог.
+
+## Результат
+
+Код скрипта для добавления новых пользователей: ![](https://github.com/NastyaP1/quantori-devops-school/blob/master/Linux_Administration/hw4/scripts/script.sh)
+
+**Демонстрация**
+
+![](https://github.com/NastyaP1/quantori-devops-school/blob/master/Linux_Administration/hw4/resources/LinuxAdm1.png)
+
+![](https://github.com/NastyaP1/quantori-devops-school/blob/master/Linux_Administration/hw4/resources/LinuxAdm2.png)
+
+![](https://github.com/NastyaP1/quantori-devops-school/blob/master/Linux_Administration/hw4/resources/LinuxAdm3.png)
+
+![](https://github.com/NastyaP1/quantori-devops-school/blob/master/Linux_Administration/hw4/resources/LinuxAdm4.png)
---
+++ Linux_Administration/hw4/scripts/script.sh
@@ -0,0 +1,80 @@
+#!/bin/bash
+
+function usage()
+{
+   cat << HEREDOC
+
+   Usage: $progname -u <username> -g <admin_group> -d <ftp_dir>
+
+   optional arguments:
+     -h                            show this help message and exit
+     -u   username                 username account
+     -g   admin_group              name of admin group
+     -d   ftp_dir                  ftp-server directory
+
+HEREDOC
+}
+
+if [ "$USER" = "root" ]
+then
+
+    HOME_DIR="/home"
+    USER_ACCOUNT=''
+    ADMIN_GROUP="ftp_admin"
+    PASSWD=''
+    FTP_DIR="/var/ftp"
+
+    while getopts u:g:d:h opt
+    do
+        case $opt in
+            u)
+                USER_ACCOUNT=${OPTARG}
+            ;;
+            g)
+                ADMIN_GROUP=${OPTARG}
+            ;;
+            d)
+                FTP_DIR=${OPTARG}
+            ;;
+            h)
+                usage
+                exit 0
+            ;;
+            *)
+                echo "ERROR: Unrecognised Option $opt"
+                usage
+                exit 1
+            ;;
+        esac
+    done
+
+    if [ -z $USER_ACCOUNT ];
+    then
+        echo "Not all mandatory options are set!"
+        usage
+        exit 1
+    fi
+
+    NOW=$(date +"%Y-%m-%d-%X")
+    LOGFILE="log-$NOW.log"
+
+    cut -d: -f1 /etc/passwd | grep "$USER_ACCOUNT" > /dev/null
+    if [ $? -eq 0 ];then
+        echo "ERROR: User account: $USER_ACCOUNT already exists."
+        echo "ERROR: User account: $USER_ACCOUNT already exists." >> "$LOGFILE"
+    else
+        /usr/sbin/useradd -d "$HOME_DIR/$USER_ACCOUNT" "$USER_ACCOUNT"
+        PASSWD=$(pwgen 10 1)
+        echo "$USER_ACCOUNT:$PASSWD" | chpasswd
+        echo "$USER_ACCOUNT $pass" >> "$LOGFILE"
+        echo "The user $USER_ACCOUNT has been created and has the password: $PASSWD"
+        mkdir $FTP_DIR/$USER_ACCOUNT
+        echo "Directory $FTP_DIR/$USER_ACCOUNT has been created" >> "$LOGFILE"
+        echo "Directory $FTP_DIR/$USER_ACCOUNT has been created"
+        sudo chown -R $USER_ACCOUNT:$ADMIN_GROUP $FTP_DIR/$USER_ACCOUNT
+      fi
+   exit 0
+else
+   echo "ERROR: You must be a root user to execute this script."
+   exit 1
+fi
