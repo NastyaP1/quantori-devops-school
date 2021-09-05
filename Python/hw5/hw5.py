9b472db6acd91819ee3ab62e9226eaf1b007e4c6
NastyaP1 <40768378+NastyaP1@users.noreply.github.com>
2021-09-05 20:25:56 +0300

    Update hw5.py

--- Python/hw5/hw5.py
+++ Python/hw5/hw5.py
@@ -7,10 +7,10 @@
 if __name__ == '__main__':
     list_of_numbers = [int(item) for item in input("Enter the list items : ").split()]
     for i in range(1, len(list_of_numbers) + 1):
-        if not i in list_of_numbers:
+        if i not in sorted(list_of_numbers):
             nextNumber = i
             break
         if i == len(list_of_numbers):
-            nextNumber = 0
+            nextNumber = list_of_numbers[-1] + 1
     print("==========================================")
     print(nextNumber)
