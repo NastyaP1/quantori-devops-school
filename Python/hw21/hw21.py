303a6ff828c487995f5b3e19a2a0a2de580229e0
NastyaP1 <40768378+NastyaP1@users.noreply.github.com>
2021-09-12 12:06:32 +0300

    Update hw21.py

--- Python/hw21/hw21.py
+++ Python/hw21/hw21.py
@@ -5,12 +5,40 @@
 # ###############################################################################
 
 
-def concatenate_list(list1, list2):
-    joined_list = []
-    joined_list += (list1 + list2)
+class NextValueWrapper(object):
+    def __init__(self, iterator):
+        self.iterator = iterator
+        self.next_value = None
+        self.finished = False
+        self.get()
 
-    return sorted(joined_list)
+    def get(self):
+        if self.finished:
+            return
+        value = self.next_value
+        try:
+            self.next_value = next(self.iterator)
+        except StopIteration:
+            self.finished = True
+        return value
+
+
+def iter_merge(iter1, iter2):
+
+    wrap1 = NextValueWrapper(iter1)
+    wrap2 = NextValueWrapper(iter2)
+
+    while not (wrap1.finished and wrap2.finished):
+        if (wrap2.finished or
+               (not wrap1.finished and
+                wrap1.next_value <= wrap2.next_value)):
+            yield wrap1.get()
+        else:
+            yield wrap2.get()
 
 
 if __name__ == '__main__':
-    print(concatenate_list([x for x in range(1, 4)], [x for x in range(2, 5)]))
+    it1 = iter([1, 3, 5, 7])
+    it2 = (x for x in range(2, 6))
+    for item in iter_merge(it1, it2):
+        print(item)
